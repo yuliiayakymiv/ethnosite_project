9b257b39b45a79e69473b1385d957505c6e76dc4
Anastasiia <kvasovets.pn@ucu.edu.ua>
2026-02-09 16:29:55 +0200

    2 version of task 2

--- ETNOSITE/app.py
+++ ETNOSITE/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
